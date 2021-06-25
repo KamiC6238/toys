2251ab3a27646db716359e5b21b229bc9412916c
danzzzz <823913645@qq.com>
2021-06-25 14:48:49 +0800

    feat: 简易的发布订阅功能

---
+++ init/src/common/subscribe.ts
@@ -0,0 +1,55 @@
+type EventCallback = Function[]
+
+type Events = {
+  [type: string]: EventCallback
+}
+
+export class PublishSubscribe {
+  private events: Events = {}
+
+  subscribe (type: string, callback: Function) {
+    if (this.events[type]) {
+      this.events[type].push(callback)
+    } else {
+      this.events[type] = [callback]
+    }
+  }
+
+  publish (type: string) {
+    if (this.events[type]) {
+      this.events[type].forEach(callback => {
+        try {
+          callback()
+        } catch (error: any) {
+          throw new Error(`callback excute error: ${error}`)
+        }
+      })
+    }
+  }
+
+  once (type: string, callback: Function) {
+    const _callback = () => {
+      callback()
+
+      this.unsubsribe(type, _callback)
+    }
+    this.subscribe(type, _callback)
+  }
+
+  unsubsribe (type: string, callback: Function) {
+    if (this.events[type]) {
+      const callbackIndex = this.events[type].findIndex(cb => cb === callback)
+
+      if (callbackIndex !== -1) {
+        this.events[type].splice(callbackIndex, 1)
+      }
+      if (this.events[type].length === 0) {
+        delete this.events[type]
+      }
+    }
+  }
+
+  unsubscribeAll () {
+    this.events = {}
+  }
+}
